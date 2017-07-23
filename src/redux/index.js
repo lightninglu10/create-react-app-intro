0fc62b873aa19f18031779e380f9095735a546bc
Patrick Lu <lightning.lu7@gmail.com>
2017-07-22 19:12:57 -0700

    initial commit for create-react-app

---
+++ src/redux/index.js
@@ -0,0 +1,12 @@
+/**
+* create-react-app-intro
+* Redux reducer index file
+* author: @
+*/
+
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+export default combineReducers({
+  routing: routerReducer,
+});
